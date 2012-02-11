e354bd693af2fafdbb3da0969359c4118e4700cb
James Bloomer <github2@thebloomers.co.uk>
2012-02-11 18:59:00 +0000

    implement playCorrectSound

---
+++ test/tadaa-test.js
@@ -0,0 +1,43 @@
+var vows = require('vows'),
+		should = require('should'),
+		sinon = require('sinon'),
+		play = require('play'),
+		tadaa = require('../lib/tadaa.js');
+
+vows.describe('Tadaa Tests')
+.addBatch({
+	'when result is greater than current' : {
+		topic : function() {
+			var stub = sinon.stub(play, 'sound').yields(null);
+			tadaa.playCorrectSound(2, 1, 'up.wav', 'down.wav', this.callback);
+		},
+		'should not error' : function(err, result) {
+			should.not.exist(err);
+		},
+		'should call play with up.wav' : function(stub) {
+			play.sound.calledWith('up.wav').should.be.true;
+		},
+	},
+	teardown: function() {
+		play.sound.restore();
+	}
+})
+.addBatch({
+	'when result is less than current' : {
+		topic : function() {
+			var stub = sinon.stub(play, 'sound').yields(null);
+			tadaa.playCorrectSound(1, 2, 'up.wav', 'down.wav', this.callback);
+		},
+		'should not error' : function(err, result) {
+			should.not.exist(err);
+		},
+		'should call play with down.wav' : function(stub) {
+			play.sound.calledWith('down.wav').should.be.true;
+		},
+	},
+	teardown: function() {
+		play.sound.restore();
+	}
+})
+.export(module);
+
