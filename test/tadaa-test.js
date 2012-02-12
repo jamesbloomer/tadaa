ac3c875fd0e91ec758ac5a01d42c9e3a9efbd7f5
James Bloomer <james@thebloomers.co.uk>
2012-02-12 11:06:15 +0000

    Dont play a sound if the current and the result are the same

--- test/tadaa-test.js
+++ test/tadaa-test.js
@@ -14,7 +14,7 @@ vows.describe('Tadaa Tests')
 		'should not error' : function(err, result) {
 			should.not.exist(err);
 		},
-		'should call play with up.wav' : function(stub) {
+		'should play up.wav' : function(stub) {
 			child.exec.calledWith('aplay up.wav').should.be.true;
 		},
 	},
@@ -31,8 +31,25 @@ vows.describe('Tadaa Tests')
 		'should not error' : function(err, result) {
 			should.not.exist(err);
 		},
-		'should call play with down.wav' : function(stub) {
+		'should play down.wav' : function(stub) {
 			child.exec.calledWith('aplay down.wav').should.be.true;
+		},
+	},
+	teardown: function() {
+		child.exec.restore();
+	}
+})
+.addBatch({
+	'when result is equal to current' : {
+		topic : function() {
+			sinon.stub(child, 'exec').yields(null);
+			tadaa.playCorrectSound(1, 1, 'up.wav', 'down.wav', this.callback);
+		},
+		'should not error' : function(err, result) {
+			should.not.exist(err);
+		},
+		'should not play a sound' : function(stub) {
+			child.exec.called.should.be.false;
 		},
 	},
 	teardown: function() {
