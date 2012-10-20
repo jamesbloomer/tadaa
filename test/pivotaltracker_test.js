2c0d020652ea373abfc18ffc97f7fe30ff50f805
James Bloomer <github2@thebloomers.co.uk>
2012-10-20 12:17:04 +0100

    rewrote tests using mocha

--- test/pivotaltracker_test.js
+++ test/pivotaltracker_test.js
@@ -1,33 +1,35 @@
-var vows = require('vows'),
-		should = require('should'),
-		sinon = require('sinon'),
-		pivotal = require("pivotal"),
-		pivotaltracker = require('../lib/pivotaltracker.js');
+var mocha = require('mocha'),
+	assert = require('assert'),
+	sinon = require('sinon'),
+	pivotal = require("pivotal"),
+	pivotaltracker = require('../lib/pivotaltracker.js');
 
-vows.describe('Tadaa Pivotal Tracker Tests')
-.addBatch({
-	'when getOpenBugs called ' : {
-		topic : function() {
+describe('Tadaa Pivotal Tracker Tests', function() {
+	describe('when getOpenBugs called', function() {
+		beforeEach(function() {
 			sinon.stub(pivotal, 'useToken');
-			sinon.stub(pivotal, 'getStories').yields(null);
-			pivotaltracker.getOpenBugs({ token: "myToken", projectId: "projectId" }, this.callback);
-		},
-		'should not error' : function(err, result) {
-			should.not.exist(err);
-		},
-		'should call pivotal methods' : function(err, result) {
-			// TODO change when token changes
-			pivotal.useToken.calledWith('myToken').should.be.true;
-			pivotal.getStories.calledWith('projectId', { filter: "type:bug state:unscheduled,unstarted,started,finished,delivered,rejected" }).should.be.true;
-		},
-		'should get correct story length' : function(err, result) {
-			// TODO
-			should.not.exist(err);
-        }
-	},
-	teardown: function() {
-		pivotal.useToken.restore();
-		pivotal.getStories.restore();
-	}
-})
-.export(module);
+			sinon.stub(pivotal, 'getStories').yields(null, { story : { length : 1 } });
+		});
+
+		afterEach(function() {
+			pivotal.useToken.restore();
+			pivotal.getStories.restore();
+		});
+
+		it('should not error', function(done) {
+			pivotaltracker.getOpenBugs({ token: "myToken", projectId: "projectId" }, function(err) {
+				assert.equal(err, null);
+				done();
+			});
+		});
+
+		it('should call pivotal methods', function(done) {
+			pivotaltracker.getOpenBugs({ token: "myToken", projectId: "projectId" }, function(err) {
+				// TODO change when token changes
+				assert(pivotal.useToken.calledWith('myToken'));
+				assert(pivotal.getStories.calledWith('projectId', { filter: "type:bug state:unscheduled,unstarted,started,finished,delivered,rejected" }));
+				done();
+			});
+		});
+	});
+});
--- test/zendesk_test.js
+++ test/zendesk_test.js
@@ -1,31 +1,41 @@
-var vows = require('vows'),
-		should = require('should'),
-		sinon = require('sinon'),
-		buddha = require("buddha"),
-		zendesk = require('../lib/zendesk.js');
+var mocha = require('mocha'),
+	assert = require('assert'),
+	sinon = require('sinon'),
+	buddha = require("buddha"),
+	zendesk = require('../lib/zendesk.js');
 
-vows.describe('Tadaa Zendesk Tests')
-.addBatch({
-	'when getOpenTickets called ' : {
-		topic : function() {
+describe('Tadaa Zendesk Tests', function() {
+	describe('when getOpenTickets called', function(done) {
+		beforeEach(function() {
 			sinon.stub(buddha, 'setCredentials');
 			sinon.stub(buddha, 'getTicketCount').yields(null, 1);
-			zendesk.getOpenTickets({ zenHost: "company.zendesk.com", zenEmail: "jon@a.com", zenPassword: "secure", zenView: "123456" }, this.callback);
-		},
-		'should not error' : function(err, result) {
-			should.not.exist(err);
-		},
-		'should call buddha methods' : function(err, result) {
-			buddha.setCredentials.calledWith('company.zendesk.com', 'jon@a.com', 'secure').should.be.true;
-			buddha.getTicketCount.calledWith('123456').should.be.true;
-		},
-		'should get correct ticket count' : function(err, result) {
-			 result.should.eql(1);
-    }
-	},
-	teardown: function() {
-		buddha.setCredentials.restore();
-		buddha.getTicketCount.restore();
-	}
-})
-.export(module);
+		});
+
+		afterEach(function() {
+			buddha.setCredentials.restore();
+			buddha.getTicketCount.restore();
+		});
+
+		it('should not error', function(done) {
+			zendesk.getOpenTickets({ zenHost: "company.zendesk.com", zenEmail: "jon@a.com", zenPassword: "secure", zenView: "123456" }, function(err) {
+				assert.equal(err, null);
+				done();
+			});
+		});
+
+		it('should call buddha methods', function(done) {
+			zendesk.getOpenTickets({ zenHost: "company.zendesk.com", zenEmail: "jon@a.com", zenPassword: "secure", zenView: "123456" }, function(err, result) {
+				assert(buddha.setCredentials.calledWith('company.zendesk.com', 'jon@a.com', 'secure'));
+				assert(buddha.getTicketCount.calledWith('123456'));
+				done();
+			});
+		});
+
+		it('should get correct ticket count', function(done) {
+			zendesk.getOpenTickets({ zenHost: "company.zendesk.com", zenEmail: "jon@a.com", zenPassword: "secure", zenView: "123456" }, function(err, result) {
+				assert.equal(result, 1);
+				done();
+			});
+		});
+	});
+});
